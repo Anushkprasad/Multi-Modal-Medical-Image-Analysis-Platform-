70d232215200f9d28654425b0e1016d0f9522523
whohrshuu <bhagia123harsh@outlook.com>
2026-08-16 04:31:18 +0530

    Add X-ray upload frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import UploadForm from "./components/UploadForm"
 import Navbar from "./components/Navbar"
 function App() {
   return (
+    <>
     <Navbar />
+    <UploadForm />
+    </>
+    
   );
 }
 
